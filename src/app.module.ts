1e6c8372ed3ab08fb4b0b200c674e133d2df0956
wangpeng1582 <wp1582@hanweb.com>
2020-06-16 18:54:44 +0800

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+import { CatsService } from './service/cats/cats.service';
+import { TestService } from './service/test/test.service';
+import { TestController } from './controllers/test/test.controller';
+import { CatsController } from './controllers/cats/cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+  controllers: [AppController, CatsController, TestController],
+  providers: [AppService, CatsService, TestService],
 })
 export class AppModule {}
